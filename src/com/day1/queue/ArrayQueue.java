be4dc38eb308bd924460bf6eb4ff8cf887e90af0
lance <x126838@126.com>
2022-02-04 23:05:44 +0800

    针对队列的实现(使用数组的方式) commit2

--- src/com/day1/queue/ArrayQueue.java
+++ src/com/day1/queue/ArrayQueue.java
@@ -9,7 +9,7 @@ package com.day1.queue;
  */
 public class ArrayQueue {
     public static void main(String[] args) {
-
+        new ArrayQueueClass(10);
     }
 }
 
@@ -26,5 +26,59 @@ class ArrayQueueClass{
     public ArrayQueueClass(int maxSize) {
         this.maxSize = maxSize;
         arr=new int[this.maxSize]; //确定数组的最大容量
+        front=-1;
+        rear=-1;
     }
+
+    //判断队列是否满
+    public boolean isFull(){
+        return rear==maxSize-1;
+    }
+    //判断队列是否为空
+    public boolean isEmpty(){
+        return rear==front;
+    }
+
+    //添加数据到队列
+    public void addQueue(int n) {
+        if (isFull())
+        {
+            System.out.println("队列满不能加入数据");
+
+        }
+        rear++;
+        arr[rear]=n;
+
+    }
+    //获取数据出队列
+    public int exportQueue(){
+        if(isEmpty()){
+            throw new RuntimeException("队列为空，无法输出");
+        }
+                return arr[++front];
+    }
+
+    //显示队列的所有数据
+    public void getAllQueue(){
+        if(isEmpty())
+        {
+            throw new RuntimeException("队列为空，无法输出");
+        }
+        for (int i:arr)
+        {
+            System.out.println("arr["+i+"]="+arr[i]);
+        }
+    }
+
+    //显示队列的头数据
+    public int headQueue(){
+        if(isEmpty())
+        {
+            throw new RuntimeException("队列为空，无法输出");
+        }
+        return arr[front+1];
+
+    }
+
+
 }
