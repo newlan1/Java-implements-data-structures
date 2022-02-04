5ee69b3be950b9b3044379782fc8a9770272a9a4
lance <x126838@126.com>
2022-02-04 22:29:53 +0800

    second commit

---
+++ src/com/day1/queue/ArrayQueue.java
@@ -0,0 +1,14 @@
+package com.day1.queue;
+
+/**
+ * @author lance
+ * @Description
+ * @create 2022-02-04 22:26
+ * @create 22:26
+ * @Version v1.0
+ */
+public class ArrayQueue {
+    public static void main(String[] args) {
+
+    }
+}
