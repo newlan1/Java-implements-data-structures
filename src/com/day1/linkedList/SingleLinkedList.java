df29010973935da40060ec813c2ca23cf6dc8c5d
lance <x126838@126.com>
2022-02-23 20:21:06 +0800

    针对单链表的实现(使用数组的方式) commit 2month23day

--- src/com/day1/linkedList/SingleLinkedList.java
+++ src/com/day1/linkedList/SingleLinkedList.java
@@ -7,65 +7,121 @@ package com.day1.linkedList;/**
 */public class SingleLinkedList {
     public static void main(String[] args) {
         SingleLinked singleLinked = new SingleLinked();
-        HeroNode heroNode1=new HeroNode(1,"李逵","1001");
-        HeroNode heroNode2=new HeroNode(3,"吴用","1002");
-        HeroNode heroNode3=new HeroNode(2,"关胜","1003");
-        HeroNode heroNode4=new HeroNode(4,"花荣","1004");
-        HeroNode heroNode5=new HeroNode(3,"宋江","1005");
-
+        HeroNode heroNode1 = new HeroNode(1, "李逵", "1001");
+        HeroNode heroNode2 = new HeroNode(3, "吴用", "1002");
+        HeroNode heroNode3 = new HeroNode(2, "关胜", "1003");
+        HeroNode heroNode4 = new HeroNode(4, "花荣", "1004");
+        HeroNode heroNode5 = new HeroNode(5, "宋江", "1005");
 
         singleLinked.add_ordered(heroNode1);
         singleLinked.add_ordered(heroNode2);
         singleLinked.add_ordered(heroNode3);
         singleLinked.add_ordered(heroNode4);
-        singleLinked.update(heroNode5);
-        singleLinked.delete(1);
+        singleLinked.add_ordered(heroNode5);
+        //singleLinked.update(heroNode5);
+        //singleLinked.delete(1);
 
         singleLinked.allList();
+
+
+        System.out.println("有效个数为:" + SingleLinkedList.getLength(singleLinked.getHead()));
+        int index=3;
+
+        //查到倒数第K个元素
+        System.out.println("查到倒数第"+index+"个元素为:" + findLastIndexNode(singleLinked.getHead(), index));
+
+
     }
+    //得到有效节点的个数
+    public static int getLength(HeroNode head) {
+        if (head.next == null) { //空链表
+            return 0;
+        }
+        int length = 0;
+        //定义一个辅助的变量, 这里我们没有统计头节点
+        HeroNode cur = head.next;
+        while (cur != null) {
+            length++;
+            cur = cur.next; //遍历
+        }
+        return length;
+    }
+    //查找倒数第K个元素的值
+    public static HeroNode findLastIndexNode(HeroNode head, int index) {
+
+
+        if (head.next == null) {
+            return null;
+        }
+
+
+        HeroNode temp = head;
+        int size = getLength(head);
+
+
+        if (index <= 0 || index > size) {
+            return null;
+        }
+
+        //定义辅助变量
+        //指向第一个有效节点
+        HeroNode cur = head.next;
+        for (int i = 0; i < size - index; i++) {
+            cur = cur.next;
+
+        }
+
+        return cur;
+
+
+    }
+
 }
 
-class SingleLinked{
 
-    private HeroNode head=new HeroNode(0,"0","0");
+
+class SingleLinked {
+
+    private HeroNode head = new HeroNode(0, "0", "0");
+
+    public HeroNode getHead() {
+        return head;
+    }
 
     //添加
-    public void add(HeroNode heroNode)
-    {
-        HeroNode temp=head;
+    public void add(HeroNode heroNode) {
+        HeroNode temp = head;
         //当循环执行完毕时 temp一定指向链表的最后
-        while (true)
-        {
+        while (true) {
 
 
             //找到了链表的最后
-            if(temp.next==null) {
+            if (temp.next == null) {
                 break;
             }
             //如果没有找到 就后移
-            temp=temp.next;
+            temp = temp.next;
         }
         //将最后的节点指向
-        temp.next=heroNode;
+        temp.next = heroNode;
     }
 
     //显示链表
-    public void allList(){
+    public void allList() {
 
         //判断一下链表是否为空
-        if(head.next==null){
+        if (head.next == null) {
 
             System.out.println("链表为空");
             return;
         }
 
 
+        HeroNode temp = head.next;
 
-        HeroNode temp=head.next;
+        while (true) {
 
-        while (true){
-
-            if(temp==null){
+            if (temp == null) {
                 break;
             }
 
@@ -73,7 +129,7 @@ class SingleLinked{
             System.out.println(temp.toString());
 
             //后移
-            temp=temp.next;
+            temp = temp.next;
 
 
         }
@@ -89,7 +145,7 @@ class SingleLinked{
         HeroNode temp = head;
         boolean flag = false;
         while (true) {
-            if(temp.next == null) {//说明temp已经在链表的最后
+            if (temp.next == null) {//说明temp已经在链表的最后
                 break; //
             }
 
@@ -109,62 +165,86 @@ class SingleLinked{
         if (flag) {
             //不能添加
             System.out.println("准备插入的英雄的标号已经存在:" + heroNode.no);
-        } else{
+        } else {
             //插入到链表中
-            heroNode.next=temp.next;
-            temp.next=heroNode;
+            heroNode.next = temp.next;
+            temp.next = heroNode;
         }
 
 
-        }
+    }
 
 
     //修改
-    public void update(HeroNode newHeroNode)
-    {
-        if(head.next==null){
+    public void update(HeroNode newHeroNode) {
+        if (head.next == null) {
             System.out.println("链表为空");
             return;
         }
 
 
-        HeroNode temp=head;
+        HeroNode temp = head;
 
-        while (true){
-            if(temp.next.no==newHeroNode.no){
-                temp.next.name= newHeroNode.name;
-                temp.next.nickname= newHeroNode.nickname;
+        while (true) {
+            if (temp.next.no == newHeroNode.no) {
+                temp.next.name = newHeroNode.name;
+                temp.next.nickname = newHeroNode.nickname;
                 return;
             }
             //后移
-            temp=temp.next;
+            temp = temp.next;
         }
 
     }
 
     //删除
-    public void delete(int id){
-           HeroNode temp=head;
-           while (true)
-           {
-               if(temp.next==null)
-               {
-                   System.out.println("无法删除 不存在！");
-                    return;
-               }
+    public void delete(int id) {
+        HeroNode temp = head;
+        while (true) {
+            if (temp.next == null) {
+                System.out.println("无法删除 不存在！");
+                return;
+            }
 
-               if(temp.next.no==id)
-               {
-                   temp.next=temp.next.next;
-                    return;
-               }
-           temp=temp.next;
-           }
+            if (temp.next.no == id) {
+                temp.next = temp.next.next;
+                return;
+            }
+            temp = temp.next;
+        }
 
     }
 
 
+    //判断单链表的节点的个数
+
+    public static int getLength(HeroNode head)
+    {
+        if(head.next==null){
+            return 0;
+        }
+
+        int length=0;
+
+        //定义一个辅助变量
+        HeroNode cur=head.next;
+        while(cur!=null)
+        {
+            length++;
+            cur=cur.next;
+        }
+        return length;
+    }
+
+
+    //新浪面试题  查找单链表中的倒数第K个节点
+
+
+
 }
+
+
+
 
 
 
